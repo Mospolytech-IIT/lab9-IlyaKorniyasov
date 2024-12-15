7b2a68fe5dcf3b18661c98bf44dfc3aa0e989a22
Ilya <buddybuddym8@mail.ru>
2024-12-15 23:57:57 +0300

    done

---
+++ update.py
@@ -0,0 +1,24 @@
+"""importing sqlalcheny and stuff"""
+from sqlalchemy.orm import sessionmaker
+from database import User, Post, engine
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def update_user_email(user_id, new_email):
+    """updating user email"""
+    user = session.query(User).filter_by(id=user_id).first()
+    if user:
+        user.email = new_email
+        session.commit()
+        return {"message": f"User {user.username} email updated to {new_email}"}
+    return {"error": "User not found"}
+
+def update_post_content(post_id, new_content):
+    """upodating post content"""
+    post = session.query(Post).filter_by(id=post_id).first()
+    if post:
+        post.content = new_content
+        session.commit()
+        return {"message": f"Post content updated for post ID {post_id}"}
+    return {"error": "Post not found"}
